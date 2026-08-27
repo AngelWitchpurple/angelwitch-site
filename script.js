45bcdcd339447b6a097152475d3de266ce8b30d1
AngelWitchpurple <maiamisqui@gmail.com>
2026-08-27 20:09:43 -0300

    Refactor smooth scroll event handling for links

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 document.querySelectorAll('a[href^="#"]').forEach(link => {
-  link.addEventListener('click', function(event) {
-    event.preventDefault();
 
-    const target = document.querySelector(this.getAttribute('href'));
+  link.addEventListener('click', function(event) {
+
+    const target = document.querySelector(
+      this.getAttribute('href')
+    );
 
     if (target) {
+
+      event.preventDefault();
+
       target.scrollIntoView({
         behavior: 'smooth'
       });
+
     }
+
   });
+
 });
