664b5eb79d87834652a4e0c570c276ef6058fcef
AngelWitchpurple <maiamisqui@gmail.com>
2026-08-27 21:02:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -19,3 +19,23 @@ document.querySelectorAll('a[href^="#"]').forEach(link => {
   });
 
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+
+  link.addEventListener('click', function(event) {
+
+    const target = document.querySelector(
+      this.getAttribute('href')
+    );
+
+    if (target) {
+      event.preventDefault();
+
+      target.scrollIntoView({
+        behavior: 'smooth'
+      });
+    }
+
+  });
+
+});
